31b970abd4c9e2889aec29735cd28f13402848e5
adi-prasetyo <37532413+adi-prasetyo@users.noreply.github.com>
2026-06-04 17:19:05 +0900

    feat(iac): author iac/checkout_assets.tf

---
+++ iac/checkout_assets.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "checkout_assets" {
+  name     = "driftscribe-hack-2026-assets"
+  project  = var.project_id
+  location = var.region
+
+  force_destroy               = false
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  labels = {
+    purpose    = "checkout-demo-assets"
+    managed-by = "driftscribe-iac"
+  }
+}
